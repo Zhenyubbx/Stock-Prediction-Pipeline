379478ee9322aeda7fde95a13b27f045d441ff56
Zhenyubbx <79489486+Zhenyubbx@users.noreply.github.com>
2023-04-18 14:22:07 +0800

    Completed loading of tweets into big query

--- DataExtraction/TwitterAPI.py
+++ DataExtraction/TwitterAPI.py
@@ -2,6 +2,7 @@ import requests
 import os
 import json
 from datetime import datetime, timedelta
+import pandas as pd
 
 current_time = datetime.utcnow()
 ten_seconds = timedelta(seconds=10)
@@ -16,7 +17,7 @@ search_url = "https://api.twitter.com/2/tweets/search/recent"
 # Optional params: start_time,end_time,since_id,until_id,max_results,next_token,
 # expansions,tweet.fields,media.fields,poll.fields,place.fields,user.fields
 query_params_17 = {'query': '#tsla', 'max_results': '100', 'tweet.fields': 'created_at,author_id',
-                   'start_time': '2023-04-17T00:00:00Z', 'end_time': end_time.strftime('%Y-%m-%dT%H:%M:%SZ')}
+                   'start_time': '2023-04-17T00:00:00Z', 'end_time': '2023-04-17T23:59:59Z'} # end_time.strftime('%Y-%m-%dT%H:%M:%SZ')
 
 
 query_params_16 = {'query': '#tsla', 'max_results': '100', 'tweet.fields': 'created_at,author_id',
@@ -38,32 +39,35 @@ def connect_to_endpoint(url, params):
     return response.json()
 
 def main():
-    # Get 20 tweets from April 17
-    json_response_17 = connect_to_endpoint(search_url, query_params_17)
-    tweets_17 = json_response_17['data']
-
-    # Get 20 tweets from April 16
+    # Get 100 tweets from April 16
     json_response_16 = connect_to_endpoint(search_url, query_params_16)
-    tweets_16 = json_response_16['data']
+    print("Successfully pulled " + str(len(json_response_16['data'])) + " from Tweepy!")
+    
+    # Get 99 tweets from April 17
+    json_response_17 = connect_to_endpoint(search_url, query_params_17)
+    print("Successfully pulled " + str(len(json_response_17['data'])) + " from Tweepy!")
     
     with open('tsla_tweets.json', mode='w') as file:
-        data = []
+        data_list = []
         
-        for tweet in tweets_17:
-            data.append({
+        for tweet in json_response_17['data']:
+            data_list.append({
                 'Time': tweet['created_at'],
                 'User': tweet['author_id'],
                 'Tweet': tweet['text']
             })
             
-        for tweet in tweets_16:
-            data.append({
+        for tweet in json_response_16['data']:
+            data_list.append({
                 'Time': tweet['created_at'],
                 'User': tweet['author_id'],
                 'Tweet': tweet['text']
             })
         
-        json.dump(data, file, indent=2)
+        json.dump(data_list, file, indent=2)
+        combined = pd.DataFrame(data=data_list)
+        combined.to_gbq("is3107-project-383009.Dataset.tslaTweets1617Apr", project_id="is3107-project-383009")
+        print(combined)
             
     with open('tsla_tweets.json') as file:
         print(file.read())
--- DataExtraction/YhFinanceAPI.py
+++ DataExtraction/YhFinanceAPI.py
@@ -2,36 +2,48 @@ import requests
 import pandas as pd
 import configparser
 # from google.cloud import bigquery
+import yfinance as yf
+from datetime import timedelta, datetime
 
+# url = "https://yh-finance.p.rapidapi.com/stock/v3/get-historical-data"
 
-url = "https://yh-finance.p.rapidapi.com/stock/v3/get-historical-data"
+# querystring = {"symbol":"meta","region":"US"} #Currently reading Meta stock data
 
-querystring = {"symbol":"meta","region":"US"} #Currently reading Meta stock data
+# config = configparser.ConfigParser()
+# config.read('config.ini')
 
-config = configparser.ConfigParser()
-config.read('config.ini')
+# headers = {
+# 	"X-RapidAPI-Key": f"{config['yhFinance']['RAPID_API_KEY']}",
+# 	"X-RapidAPI-Host": "yh-finance.p.rapidapi.com"
+# }
 
-headers = {
-	"X-RapidAPI-Key": f"{config['yhFinance']['RAPID_API_KEY']}",
-	"X-RapidAPI-Host": "yh-finance.p.rapidapi.com"
-}
+# response = requests.request("GET", url, headers=headers, params=querystring)
 
-response = requests.request("GET", url, headers=headers, params=querystring)
+# Set start & end date of stock market data
+end_date = datetime.now()
+start_date = end_date - timedelta(days=15*365)
 
-responseJson = response.json()
-filtered_data = []
-for day in responseJson["prices"]:
-    day_data = {
-                'date': day['date'], #Date in number of seconds since epoch
-                'open': day['open'],
-                'close': day['close'],
-                'volume': day['volume'],
-                }
-    filtered_data.append(day_data)
+# Download data from Yahoo Finance
+history = yf.download("tsla", start=start_date, end=end_date, interval='1d', prepost=False)
+history = history.loc[:, ['Open', 'Close', 'Volume']]
+history.reset_index(inplace=True)
+history.to_gbq("is3107-project-383009.Dataset.tslaStock", project_id="is3107-project-383009")
 
-df = pd.DataFrame(data=filtered_data)
-df.to_csv("YhFinance.csv", index=False)
-df.to_gbq("is3107-project-383009.Dataset.test2", project_id="is3107-project-383009")
+# responseJson = response.json()
+# filtered_data = []
+# for day in responseJson["prices"]:
+#     day_data = {
+#                 'date': day['date'], #Date in number of seconds since epoch
+#                 'open': day['open'],
+#                 'close': day['close'],
+#                 'volume': day['volume'],
+#                 }
+#     filtered_data.append(day_data)
+
+# df = pd.DataFrame(data=filtered_data)
+# print(df)
+# df.to_csv("YhFinance.csv", index=False)
+# df.to_gbq("is3107-project-383009.Dataset.test2", project_id="is3107-project-383009")
 
 # Alternative way to push data to BigQuery
 
