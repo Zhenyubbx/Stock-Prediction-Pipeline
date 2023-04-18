f83bfb6bc7689e5dac30f67be27349c245594241
Zhenyubbx <79489486+Zhenyubbx@users.noreply.github.com>
2023-04-18 14:55:51 +0800

    Added cleaning data in TwitterAPI

--- DataExtraction/TwitterAPI.py
+++ DataExtraction/TwitterAPI.py
@@ -3,6 +3,7 @@ import os
 import json
 from datetime import datetime, timedelta
 import pandas as pd
+import re
 
 current_time = datetime.utcnow()
 ten_seconds = timedelta(seconds=10)
@@ -51,24 +52,38 @@ def main():
         data_list = []
         
         for tweet in json_response_17['data']:
+            date = datetime.strptime(str(tweet['created_at'].replace("Z", "")), '%Y-%m-%dT%H:%M:%S.000')
             data_list.append({
-                'Time': tweet['created_at'],
+                'Date': str(date.strftime('%Y-%m-%d')),
                 'User': tweet['author_id'],
                 'Tweet': tweet['text']
             })
             
         for tweet in json_response_16['data']:
+            date = datetime.strptime(str(tweet['created_at'].replace("Z", "")), '%Y-%m-%dT%H:%M:%S.000')
             data_list.append({
-                'Time': tweet['created_at'],
+                'Date': str(date.strftime('%Y-%m-%d')),
                 'User': tweet['author_id'],
                 'Tweet': tweet['text']
             })
-        
-        json.dump(data_list, file, indent=2)
+
         combined = pd.DataFrame(data=data_list)
-        combined.to_gbq("is3107-project-383009.Dataset.tslaTweets1617Apr", project_id="is3107-project-383009")
+
+        def cleanText(text):
+            text = re.sub(r'@[A-Za-z0-9]+', '', text) #Removes @mentions
+            text = re.sub(r'#', '', text) #Removes the # symbol
+            text = re.sub(r'RT[\s]+', '', text) #Removes RT
+            text = re.sub(r'https?:\/\/\S+','', text) #Removes hyperlinks
+            #not cleaning emojis as the vader sentiment analysis takes those into account 
+
+            return text
+
+        #Clean text and return
+        combined['Tweet'] = combined['Tweet'].astype(str) #Change the tweet data type from object to string
+        combined['Tweet'] = combined['Tweet'].apply(cleanText)
         print(combined)
-            
+        combined.to_gbq("is3107-project-383009.Dataset.tslaTweets1617Apr", project_id="is3107-project-383009")
+    
     with open('tsla_tweets.json') as file:
         print(file.read())
             
