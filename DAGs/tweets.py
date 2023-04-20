a1365963c71a06a2f1f660f000be5933df79da7c
Darryl <darryl@r-54-103-25-172.comp.nus.edu.sg>
2023-04-20 16:14:45 +0800

    Added function to processTweets from top 25 companies

--- DAGs/tweets.py
+++ DAGs/tweets.py
@@ -86,6 +86,7 @@ def process_tweets():
             text = re.sub(r'#', '', text) #Removes the # symbol
             text = re.sub(r'RT[\s]+', '', text) #Removes RT
             text = re.sub(r'https?:\/\/\S+','', text) #Removes hyperlinks
+
             #not cleaning emojis as the vader sentiment analysis takes those into account 
 
             return text
@@ -94,27 +95,30 @@ def process_tweets():
         combined['Tweet Text'] = combined['Tweet Text'].astype(str) #Change the tweet data type from object to string
         combined['Tweet Text'] = combined['Tweet Text'].apply(cleanText)
         combined.drop_duplicates(subset=['Tweet Text'], inplace=True)
-        combined.to_gbq("is3107-project-383009.Dataset.tslaTweetsRealTime2", project_id="is3107-project-383009")
+        combined.reset_index(inplace=False)
+        combined = combined.rename(columns={"Tweet Text": "Tweet", "Date & Time" : "Date"})
+        # combined.to_gbq("is3107-project-383009.Dataset.tslaTweetsRealTime2", project_id="is3107-project-383009")
 
         print(combined)
 
-
         def sentiment_score_transform(df):
-            df['Tweet Text'] = df['Tweet Text'].astype(str)
-            df['Date & Time'] = df['Date & Time'].astype(str)
-            df.dropna(subset=['Tweet Text', 'Date & Time'], inplace=False)
-            df["Tweet Text"] = df["Tweet Text"].str.strip()
+            # df['Tweet Text'] = df['Tweet Text'].astype(str)
+            # df['Date & Time'] = df['Date & Time'].astype(str)
+            df.dropna(subset=['Tweet', 'Date'], inplace=True)
+            df["Tweet"] = df["Tweet"].str.strip()
             df.reset_index(inplace=False)
+            df["Date"] = pd.to_datetime(df["Date"])
             sia = SentimentIntensityAnalyzer()
-            df['Sentiments'] = df['Tweet Text'].apply(lambda Tweet: sia.polarity_scores(Tweet))
+            df['Sentiments'] = df['Tweet'].apply(lambda Tweet: sia.polarity_scores(Tweet))
             df = pd.concat([df.drop(['Sentiments'], axis=1), df['Sentiments'].apply(pd.Series)], axis=1)
-            df.to_gbq("is3107-project-383009.Dataset.realTimeAnalysed10", project_id="is3107-project-383009")
+            print(df)
+            df.to_gbq("is3107-project-383009.Dataset.realTimeAnalysed999", project_id="is3107-project-383009", if_exists='replace')
             print("successfully LOADED!")
 
         sentiment_score_transform(combined)
 
 
-        
+
 
 
 
@@ -152,9 +156,9 @@ dag = DAG(
 )
 
 
-##########################################
-#3. DEFINE AIRFLOW OPERATORS
-##########################################
+# ##########################################
+# #3. DEFINE AIRFLOW OPERATORS
+# ##########################################
 
 
 with dag:
@@ -170,17 +174,5 @@ with dag:
 
 process_tweets_task 
 download_stock_data_task
-
-
-
-
-# Task 1: process and clean tweets
-# Task 2: push to big query
-# Task 3: take from bigquery in json format
-# Task 4: twitter sentiment analysis
-# Task 5: machine learning
-# Task 6:  
-
-
 
 
--- DataExtraction/README.md
+++ DataExtraction/README.md
@@ -4,3 +4,9 @@ pip install textblob
 pip install langdetect
 pip install matplotlib
 pip install pandas.gbq
+
+BATCH DATA TICKERS:
+
+['TSLA' 'MSFT' 'PG' 'META' 'AMZN' 'GOOG' 'AMD' 'AAPL' 'NFLX' 'TSM' 'KO'
+'F' 'COST' 'DIS' 'VZ' 'CRM' 'INTC' 'BA' 'BX' 'NOC' 'PYPL' 'ENPH' 'NIO'
+'ZS' 'XPEV']
